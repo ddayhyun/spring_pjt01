c225e02cb5444677746d6fc708ffef2dc7769ed7
ddayhyun <ddayhyun1121@gmail.com>
2024-09-09 16:28:29 +0900

    이름 수정

--- src/main/java/com/example/spring_pjt01/bbs/dao/BbsMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.spring_pjt01.bbs.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public class BbsMapper {
-    
-}
---
+++ src/main/java/com/example/spring_pjt01/blog/dao/BlogMapper.java
@@ -0,0 +1,8 @@
+package com.example.spring_pjt01.blog.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class BlogMapper {
+    
+}
